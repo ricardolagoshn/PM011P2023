b8753646ce3433dd9b71410ff90a5cc745769a44
ricardo.lagos <ricardo.lagoshn@gmail.com>
2023-02-01 20:49:09 -0600

    Cambio realizado como ejemplo de un push en GitHub

---
+++ app/src/main/java/com/example/pm011p2023/ActivityCombo.java
@@ -0,0 +1,14 @@
+package com.example.pm011p2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityCombo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_combo);
+    }
+}

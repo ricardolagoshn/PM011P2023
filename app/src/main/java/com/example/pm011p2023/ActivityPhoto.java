5757cc2a7b39f0c9f9a28655a2eb372afcb2b002
ricardo.lagos <ricardo.lagoshn@gmail.com>
2023-02-14 20:46:53 -0600

    Cambios del dia 14/02/2023

--- app/src/main/java/com/example/pm011p2023/ActivityPhoto.java
+++ app/src/main/java/com/example/pm011p2023/ActivityPhoto.java
@@ -5,17 +5,25 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ActivityPhoto extends AppCompatActivity {
 
@@ -24,6 +32,7 @@ public class ActivityPhoto extends AppCompatActivity {
 
     ImageView imageView;
     Button btntakefoto;
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +73,8 @@ public class ActivityPhoto extends AppCompatActivity {
         {
             if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
             {
-                TomarFoto();
+                dispatchTakePictureIntent();
+                //TomarFoto();
             }
             else
             {
@@ -89,9 +99,50 @@ public class ActivityPhoto extends AppCompatActivity {
 
         if(requestCode == REQUEST_IMAGE)
         {
-            Bundle extra = data.getExtras();
-            Bitmap imagen = (Bitmap) extra.get("data");
-            imageView.setImageBitmap(imagen);
+            //Bundle extra = data.getExtras();
+            //Bitmap imagen = (Bitmap) extra.get("data");
+            //imageView.setImageBitmap(imagen);
+
+            File foto = new File(currentPhotoPath);
+            imageView.setImageURI(Uri.fromFile(foto));
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+            ex.toString();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.pm011p2023.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoFile);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE);
+            }
         }
     }
 }
